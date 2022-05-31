a062be6d342fb024355ac18225892be3fa7b8c88
Amna <amna.zubair2000@gmail.com>
2022-05-31 19:39:21 +0500

    my commit

---
+++ app/src/main/java/com/labsession/signinandsignup/Contants.java
@@ -0,0 +1,10 @@
+package com.labsession.signinandsignup;
+public class Contants {
+
+    private static final String ROOT_URL = "http://10.0.2.2/android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
